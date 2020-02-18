0f06472a785d53ecb420b8481fecb274180092ad
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-18 10:40:20 +0200

    Added requires and constants

--- public/index.php
+++ public/index.php
@@ -1,12 +1,20 @@
 <?php
+// $scriptfile = "main.js";
+define("CONSTANT", "Hello world.");
+const CON2 = "Hello RCS!";
+
 require_once '../src/util.php';
+require '../src/template/head.php';
+
+echo CON2;
+echo CONSTANT;
 $myname = "Valdis";
 $favorite = "potatoes";
 $num = 42;
-
+require '../src/template/header.php';
 add2(5, 10);
 
-echo "The number is $num";
+echo "The number is $num and " . CONSTANT;
 echo "<hr>";
 echo "The number is " . ($num + 10);
 
@@ -17,6 +25,7 @@ echo "<hr>";
 
 echo "I am " . $myname . " and I like " . $favorite;
 echo "<hr>";
+require '../src/template/footer.php';
 //we use . not + for string concatenation
 //below will not work!
 // echo "I am "+$myname+" and I like "+$favorite;
