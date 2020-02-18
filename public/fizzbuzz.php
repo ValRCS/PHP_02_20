cca400f65c7fa0187426e3be862847e259d9d859
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-18 12:10:22 +0200

    Fizzbuzz ordered list

--- public/fizzbuzz.php
+++ public/fizzbuzz.php
@@ -1,3 +1,21 @@
 <?php
+require "../src/template/head.php";
 //write Fizzbuzz to show each item as li in an ol
 //So loop and then if elseif else inside
+echo "<h1>FizzBuzz</h1>";
+
+echo "<ol>";
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "<li>Fizzbuzz</li>";
+    } elseif ($i % 3 == 0) {
+        echo "<li>Fizz</li>";
+    } elseif ($i % 5 == 0) {
+        echo "<li>Buzz</li>";
+    } else {
+        echo "<li>Number: $i</li>";
+    }
+
+}
+echo "</ol>";
+require "../src/template/footer.php";
