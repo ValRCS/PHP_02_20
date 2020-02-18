2f3c65e2293eb825fb4fafe7b00044e95f5a45cd
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-18 11:48:30 +0200

    If example

--- public/loops.php
+++ public/loops.php
@@ -6,7 +6,18 @@ $myfood = ["Bread", "Milk", "Potatoes", "Brocolli"];
 var_dump($myfood);
 echo "<hr>";
 
+echo "<ol class='mycool-list'>";
 foreach ($myfood as $value) {
-    echo "My food is $value";
+    echo "<li>My food is $value</li>";
     //do more stuff
+}
+echo "</ol>";
+
+$a = 5;
+if ($a > 10) {
+    echo "Bigger than 10";
+} elseif ($a > 5) {
+    echo "Bigger than 5";
+} else {
+    echo "Equal of smaller than 5";
 }
