358313863a3ad10be9d78471523900ccde83f764
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-20 12:33:16 +0200

    Added Styling and print Songs to Screen

--- src/classes/View.php
+++ src/classes/View.php
@@ -10,9 +10,30 @@ class View
     public function printSongs($songs)
     {
         echo "<hr>Printing songs</br>";
-        foreach ($songs as $song) {
-            echo "<br>";
-            print_r($song);
+        // foreach ($songs as $song) {
+        //     echo "<br>";
+        //     print_r($song);
+        // }
+        echo "<hr>";
+        $areColumnsSet = false;
+
+        foreach ($songs as $index => $row) {
+            if (!$areColumnsSet) {
+                echo "<div class='tracks-header-cont'>";
+                foreach ($row as $colname => $cell) {
+                    echo "<span class='col-fields'>$colname</span>";
+                }
+                echo "</div>";
+                $areColumnsSet = true;
+            }
+
+            echo "<div class='tracks-cont'>";
+            // echo "Row: $index";
+            // print_r($row);
+            foreach ($row as $colname => $cell) {
+                echo "<span class='track-cell'>$cell</span>";
+            }
+            echo "</div>";
         }
     }
 
