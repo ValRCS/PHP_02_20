ffe9fcb21dd83711fc2905707ebdf5b0a0385c5e
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-25 13:02:47 +0200

    Added monolog include

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
+require_once "../config/monolog.php";
 require_once "../config/config.php";
+
 require_once "../src/classes/Model.php";
 require_once "../src/classes/View.php";
 require_once "../src/classes/Controller.php";
